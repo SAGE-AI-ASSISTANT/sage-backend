2ff3fb6cce204a34dd37cf6991b43aea63b9a6bb
Nengak Dakup <nengakdakup@gmail.com>
2024-06-17 17:40:36 +0100

    Added Course Model

---
+++ models/Course.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const CourseSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    files: [{
+        type: String,
+    }],
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const Course = mongoose.model('courses', CourseSchema);
+module.exports = Course;
